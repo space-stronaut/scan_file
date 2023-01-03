84942d9ce905bd877cd3dabc93f5ff618476158e
Ronald Abel <ronald.hermansyah@kinersia.com>
2023-01-03 11:24:06 +0700

    initial commit

---
+++ check_single_directory.sh
@@ -0,0 +1,8 @@
+## check file in single directory
+
+for file in `ls`
+do
+        echo "==================== $file ===================="
+        cat $file | grep "grep"
+        echo "==================== DONE ===================="
+done
